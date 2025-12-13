4239ec9bebca883c523caa1107699983a2100c63
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-13 15:39:31 +0330

    fixed style Button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -6,15 +6,15 @@ import { cn } from '@utils/cn';
 
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-sansMedium font-medium  ring-offset-bgk transition-colors focus-visible:outline-none focus-visible:ring-2 focus-ring-ring focus-visible-ring-offset-2 disabled-pointer-events-none disabled-opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+    'inline-flex items-center justify-center gap-2  cursor-pointer whitespace-nowrap rounded-md text-sm font-sansMedium font-medium  ring-offset-bgk transition-colors focus-visible:outline-none focus-visible:ring-2 focus-ring-ring focus-visible-ring-offset-2 disabled-pointer-events-none disabled-opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
     {
         variants:{
             variant:{
                 default:"bg-primary text-primary-foreground hover:bg-primary/90",
                 destructive:"bg-danger text-white hover:bg-danger/90",
-                outline:"border border-border bg-bgk hover:copper hover-text-white",
+                outline:"border border-border bg-bgk hover:bg-copper hover:text-white",
                 secondary:"bg-secondary text-secondary-foreground hover:bg-secondary/80",
-                ghost:"hover:bg-accent hover:text-white",
+                ghost:"hover:bg-copper hover:text-white",
                 link:"text-primary underline-offset-4 hover:underline"
             },
             size:{
