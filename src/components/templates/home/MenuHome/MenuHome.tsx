587e65075e579b48544344afd47e5930af3812fc
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-14 14:57:04 +0330

    created MenuHome-page

---
+++ src/components/templates/home/MenuHome/MenuHome.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ProductCard from '@components/modules/ProductCard/ProductCard';
+import { NavLink } from 'react-router-dom';
+import { Button } from '@components/UI/Button';
+import { ArrowLeft } from 'lucide-react';
+
+const MenuHome:React.FC = ()=>{
+  return (
+    <section className="py-16 bg-bgk ">
+        <div className="container px-8">
+            <div className="mb-10 text-center">
+                <h2 className="mb-4 text-3xl font-sansBold font-bold">منو پرطرفدار</h2>
+                <p className="text-lg text-muted-foreground">محبوب ترین نوشیدنی ها و دسرهای ما</p>
+            </div>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+            </div>
+            <div className="mt-10 text-center">
+                <NavLink to='/Menu'>
+                    <Button variant='outline' size='lg' className='font-extrabold font-sansBold'>
+                        مشاهده کامل منو
+                        <ArrowLeft className='mr-2 h-5 w-5'/>
+                    </Button>
+                </NavLink>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default MenuHome
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navigation from '@components/modules/Navigation/Navigation'
 import Hero from '@components/templates/home/Hero/Hero'
 import Features from '@components/templates/home/Features/Features'
+import MenuHome from '@components/templates/home/MenuHome/MenuHome'
 
 
 const Home:React.FC = ()=>{
@@ -10,6 +11,7 @@ const Home:React.FC = ()=>{
     <Navigation/>
     <Hero/>
     <Features/>
+    <MenuHome/>
     </>
   )
 }
