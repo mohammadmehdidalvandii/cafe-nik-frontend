08d17eddd8fb18e4542f6f308f22fe1f4aa44b90
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-15 15:09:48 +0330

    created BranchHeader

---
+++ src/components/templates/Branches/BranchHeader/BranchHeader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BranchHeader:React.FC = ()=>{
+  return (
+    <section className="py-10">
+        <div className="container px-16">
+            <div className="mb10 text-center">
+                <h1 className="mb-4 text-4xl font-bold">شعب کافی شاپ نیک</h1>
+                <p className="text-muted-foreground">25 شعبه در سراسر ایران خدمت شما </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default BranchHeader
--- src/pages/Branches/Branches.tsx
+++ src/pages/Branches/Branches.tsx
@@ -1,8 +1,11 @@
+import BranchHeader from '@components/templates/Branches/BranchHeader/BranchHeader'
 import React from 'react'
 
 const Branches:React.FC = ()=>{
   return (
-    <div>Branches</div>
+   <>
+   <BranchHeader/>
+   </>
   )
 }
 
