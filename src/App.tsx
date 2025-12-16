c6ab98a893352df09c7c6a7787b1e579069490bf
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-16 15:18:20 +0330

    created page auth/register

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const Home = lazy(()=>import('@pages/Home/Home'));
 const Menu = lazy(()=>import('@pages/Menu/Menu'));
 const Branches = lazy(()=>import('@pages/Branches/Branches'));
 const Cart = lazy(()=>import('@pages/Cart/Cart'));
+const Register = lazy(()=>import('@pages/Auth/Register/Register'));
 
 const App:React.FC = ()=>{
   return (
@@ -16,6 +17,7 @@ const App:React.FC = ()=>{
       <ToasterProvider/>
         <Suspense fallback={<h1>لطفا منتظر باشید</h1>}>
           <Routes>
+            <Route path='/Auth/Register' element={<Register/>}/>
             <Route path='/' element={<MainLayout/>}>
             <Route index element={<Home/>}/>
             <Route path='Menu' element={<Menu/>}/>
