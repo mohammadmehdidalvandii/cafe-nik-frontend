560ad2fdc4460185b51ec18fc86e3d15d0d0dbc1
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-16 14:55:49 +0330

    created ui input

---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { cn } from '@utils/cn';
+
+const Input = React.forwardRef<HTMLInputElement , React.ComponentProps<'input'>>(({className , type , ...props},ref)=>{
+    return(
+        <input
+            type={type}
+            className={cn(
+                'flex h-10 w-full rounded-sm border border-border bg-bgk px-3 py-2 text-base ring-offset-bgk file:bg-transparent file:text-sm file:font-bold file:text-secondary-foreground placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-border focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                className
+            )}
+            ref={ref}
+            {...props}
+        />
+    )
+});
+
+Input.displayName = 'Input';
+export {Input}
