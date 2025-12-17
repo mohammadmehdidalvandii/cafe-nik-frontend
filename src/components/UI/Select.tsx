c1e37db2bd2ce5383c7b697c9222dc1de514ca97
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-17 15:42:57 +0330

    fixed style select ui

--- src/components/UI/Select.tsx
+++ src/components/UI/Select.tsx
@@ -14,7 +14,7 @@ React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
     <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-        'flex h-10 w-full items-center justify-between rounded-md border border-border bg-secondary/30 px-3 py-2 text-sm ring-offset-bgk placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-border focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'flex h-10 w-full items-center justify-between rounded-md border border-border bg-secondary/30 px-3 py-2 text-sm ring-offset-bgk placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-border focus:ring-offset-2 cursor-pointer disabled:opacity-50',
         className
     )}
     {...props}
@@ -67,7 +67,7 @@ React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
         <SelectPrimitive.Content
             ref={ref}
             className={cn(
-                'relative z-50 max-h-96 min-w-32 overflow-hidden rounded-md bg-[#261D17]  text-white shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-from-bottom-2',
+                'relative z-50 max-h-96 min-w-32 overflow-hidden rounded-md bg-white  text-black shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-from-bottom-2',
                 position === 'popper'&& 
                 'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
                 className
@@ -98,7 +98,7 @@ SelectContent.displayName = SelectPrimitive.Content.displayName;
     <SelectPrimitive.Item
         ref={ref}
         className={cn(
-            "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 focus:bg-copper/10 ",
+            "relative flex w-full cursor-pointer font-bold text-[0.9rem]  select-none items-center rounded-sm py-1.5 pl-8 pr-2  outline-none  hover:bg-copper hover:text-primary-foreground",
             className
         )}
         {...props}
