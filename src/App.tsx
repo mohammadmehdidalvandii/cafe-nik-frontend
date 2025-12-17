04020e7b2db27495d5125f2f71d4003db088b932
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-17 10:48:51 +0330

    created Admin/Orders page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const Register = lazy(()=>import('@pages/Auth/Register/Register'));
 const Login = lazy(()=>import('@pages/Auth/Login/Login'));
 // admin
 const Admin = lazy(()=>import('@pages/Admin/Admin'));
+const AdminOrders = lazy(()=>import('@pages/Admin/Orders/Orders'));
 
 const App:React.FC = ()=>{
   return (
@@ -35,6 +36,7 @@ const App:React.FC = ()=>{
             {/* Admin */}
             <Route path='/Admin' element={<AdminLayout/>}>
               <Route index element={<Admin/>}/>
+              <Route path='Orders' element={<AdminOrders/>}/>
             </Route>
           </Routes>
         </Suspense>
