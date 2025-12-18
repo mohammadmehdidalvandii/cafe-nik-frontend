cafa8a2ec8881d4c20cf50dcfab5eea1aa45b22e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-18 14:37:01 +0330

    created section settingSecurity for admin/security

---
+++ src/components/templates/admin/SettingSecurity/SettingSecurity.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@components/UI/Button";
+import { Input } from "@components/UI/Input";
+import { Label } from "@components/UI/Label";
+import { Shield } from "lucide-react";
+import React from "react";
+
+const SettingSecurity: React.FC = () => {
+  return (
+    <div className="rounded-xl bg-white p-6 shadow-md">
+      <form action="#">
+        <div className="mb-4 flex items-center gap-3">
+          <Shield className="h-5 w-5 text-primary" />
+          <h3 className="text-2xl font-bold">امنیت</h3>
+        </div>
+        <div className="space-y-4">
+          <div>
+            <Label htmlFor="current-password" className="text-lg">رمز عبور فعلی</Label>
+            <Input
+                id="current-password"
+                type="password"
+                className="mt-2"
+            />
+          </div>
+          <div>
+            <Label htmlFor="new-password" className="text-lg">رمز عبور جدید</Label>
+            <Input 
+                id="new-password"
+                type="password"
+                className="mt-2"
+            />
+          </div>
+          <Button className="w-full">تغییر رمز عبور</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SettingSecurity;
--- src/pages/Admin/Settings/Settings.tsx
+++ src/pages/Admin/Settings/Settings.tsx
@@ -1,5 +1,6 @@
 import SettingNotifications from '@components/templates/admin/SettingNotifications/SettingNotifications'
 import SettingProfile from '@components/templates/admin/SettingProfile/SettingProfile'
+import SettingSecurity from '@components/templates/admin/SettingSecurity/SettingSecurity'
 import React from 'react'
 
 const Settings:React.FC = ()=>{
@@ -8,6 +9,7 @@ const Settings:React.FC = ()=>{
         <SettingProfile/>
         <div className="grid gap-6 lg:grid-cols-2 mt-8">
             <SettingNotifications/>
+            <SettingSecurity/>
         </div>
     </div>
   )
