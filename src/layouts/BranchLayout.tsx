ee5a7cf29d95183712d3cc0891f672868a625e44
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-18 15:39:39 +0330

    created BrachLayout

---
+++ src/layouts/BranchLayout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const BranchLayout:React.FC = ()=>{
+  return (
+    <div className="min-h-screen">
+        {/* Desktop Sidebar */}
+        <div className="hidden lg:block">
+            this is sidebar
+        </div>
+        {/* Mobile Header */}
+        <header className="fixed left-0 right-0 top-0 flex h-16 items-center justify-between border-b border-border bg-white px-4 lg:hidden"></header>
+        {/* Main Content */}
+        <div className="min-h-screen pt-16 lg:mr-64 lg:pt-0">
+            <div className="p-4 lg:p-8">
+                <Outlet/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BranchLayout
