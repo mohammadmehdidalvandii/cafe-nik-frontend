84e9c67e2e9c5b94be18cb23c8999c1458f2a56c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-20 11:32:39 +0330

    fixed style input

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -6,7 +6,7 @@ const Input = React.forwardRef<HTMLInputElement , React.ComponentProps<'input'>>
         <input
             type={type}
             className={cn(
-                'flex h-10 w-full rounded-lg border border-border bg-bgk px-3 py-2 text-base ring-offset-bgk file:bg-transparent file:text-sm file:font-bold file:text-secondary-foreground placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-border focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                'flex h-10 w-full rounded-lg border border-border bg-bgk px-3 py-2 text-base ring-offset-bgk file:bg-transparent file:text-sm file:font-bold file:text-secondary-foreground placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-primary outline-none   focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
                 className
             )}
             ref={ref}
