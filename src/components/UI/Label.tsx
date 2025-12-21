9263b97e5ce4f5e02554709bc258330d70387f7d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-21 10:21:28 +0330

    updated style ui label

--- src/components/UI/Label.tsx
+++ src/components/UI/Label.tsx
@@ -10,7 +10,7 @@ React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
     <LabelPrimitive.Root
         ref={ref}
         className={cn(
-            'text-sm font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+            'text-lg font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
             className
         )}
         {...props}
