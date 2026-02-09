8acf53b3ce25f3e012470351dad2c4393407c15e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2026-02-09 09:13:15 +0330

    show state for adminState

--- src/components/templates/admin/AdminState/AdminState.tsx
+++ src/components/templates/admin/AdminState/AdminState.tsx
@@ -1,42 +1,59 @@
+import { useGetAllBranches } from '@services/branch.services';
+import { useGetAllCustomer } from '@services/customers.services'
+import { useGetAllOrders } from '@services/orders.services';
 import { CheckCircle, Clock, ShoppingBag, Store, TrendingUp, Users } from 'lucide-react'
 import React from 'react'
 
 const  AdminState:React.FC = ()=>{
+    const {data:Customers} = useGetAllCustomer();
+    const {data:Branches} = useGetAllBranches();
+    const {data:Orders} = useGetAllOrders();
+    console.log("orders =>" , Orders)
+    const customerCount = Customers?.length ?? 0;
+    const branchesCount = Branches?.length ?? 0;
+    const ordersCount = Orders?.length ?? 0;
+
+    const incomeAll = Orders?.reduce((sum:number , order:any)=>sum + (order.total_price || 0) , 0) ?? 0;
+
+    const orderPending = Orders?.filter((order:any)=>order.status === 'در انتظار تایید');
+    const orderPendingCount = orderPending?.length ?? 0
+    const orderCompleted =  Orders?.filter((order:any)=>order.status === "تحویل داد شد");
+    const orderComPletedCount = orderCompleted?.length ?? 0;
 
     const state = [
         {
             label:"کل سفارش ها",
-            value:`${(0).toLocaleString('fa-IR')}`,
+            value:`${ordersCount.toLocaleString('fa-IR')}`,
             icon:ShoppingBag,
             color:"bg-primary-/10 text-primary",
         },
         {
             label:"کل درآمد",
-            value:`${(120000).toLocaleString('fa-IR')}`,
+            value:`${incomeAll  .toLocaleString('fa-IR')}`,
             icon:TrendingUp,
             color:"bg-green-100 text-green-600",
         },
         {
             label:"تعداد شعب",
-            value:`${(25).toLocaleString('fa-IR')}`,
+            value:`${branchesCount.toLocaleString('fa-IR')}`,
             icon:Store,
             color:"bg-copper/10 text-copper",
         },
         {
             label:"تعداد مشتریان",
-            value:`${(100).toLocaleString('fa-IR')}`,
+            value:`${customerCount.toLocaleString('fa-IR')}`,
             icon:Users,
             color:"bg-blue-100 text-blue-600",
         },
         {
             label:"در انتظار تایید",
-            value:`${(15).toLocaleString('fa-IR')}`,
+            value:`${orderPendingCount.toLocaleString('fa-IR')}`,
             icon:Clock,
             color:"bg-yellow-100 text-yellow-600",
         },
         {
             label:"تحویل داد شده",
-            value:`${(12).toLocaleString('fa-IR')}`,
+            value:`${orderComPletedCount.toLocaleString('fa-IR')}`,
             icon:CheckCircle,
             color:"bg-emerald-100 text-emerald-600",
         }
