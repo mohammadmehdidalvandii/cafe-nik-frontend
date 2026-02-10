af0c0f18428e77f0886a4b480635ec17361fdb62
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2026-02-10 10:35:30 +0330

    fixed table completed

--- src/components/templates/branchPanel/BranchTableIncome/BranchTableIncome.tsx
+++ src/components/templates/branchPanel/BranchTableIncome/BranchTableIncome.tsx
@@ -1,6 +1,11 @@
+import { useGetOrdersBranch } from '@services/branch.services'
 import React from 'react'
+import { BranchOrderProps } from 'types/branch';
 
 const BranchTableIncome:React.FC = ()=>{
+    const {data:Orders} = useGetOrdersBranch();
+    const orderCompleted = Orders?.order?.filter((od:any)=>od.status === 'تحویل داد شد');
+    console.log(orderCompleted)
   return (
     <section className="rounded-xl bg-white p-6 shadow-md mt-8">
         <h3 className="text-lg font-bold mb-4">آخرین سفارشات تکمیل شده</h3>
@@ -8,18 +13,19 @@ const BranchTableIncome:React.FC = ()=>{
         <p className="text-muted-foreground text-center py-8">
             هنوز سفارش تکمیل شده ای وجود نداره
         </p>
-
-        <div className="flex items-center justify-between bg-muted/50 rounded-lg p-4">
+        {orderCompleted?.map((order:BranchOrderProps)=>(
+        <div className="flex items-center justify-between bg-muted/50 rounded-lg p-4" key={order.id}>
             <div>
-                <span className="text-2xl font-medium">مدیرشعبه ونک</span>
+                <span className="text-2xl font-medium">{order.user.username} | {order.user.phone}</span>
                 <p className="text-lg text-muted-foreground">
-                    {new Date().toLocaleDateString('fa-IR')}
+                    {order.delivery_date} | {order.delivery_time}
                 </p>
             </div>
             <span className="font-bold text-xl text-copper">
-                {(97500).toLocaleString('fa-IR')}
+                {order.total_price.toLocaleString('fa-IR')}
             </span>
         </div>
+        ))}
     </section>
   )
 }
