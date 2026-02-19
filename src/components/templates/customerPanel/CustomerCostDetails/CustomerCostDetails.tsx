963bc7846fa91acc6c529b5e2a32b03fb89e57c1
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2026-02-19 09:45:47 +0330

    fixed cost datiles

--- src/components/templates/customerPanel/CustomerCostDetails/CustomerCostDetails.tsx
+++ src/components/templates/customerPanel/CustomerCostDetails/CustomerCostDetails.tsx
@@ -1,23 +1,51 @@
+import { useGetOrderUser } from '@services/orders.services'
 import React from 'react'
+import { OrdersProps } from 'types/orders'
 
 const CustomerCostDetails:React.FC = ()=>{
+     const { data: Orders } = useGetOrderUser();
+    
+     const monthlyTotal = React.useMemo(() => {
+  if (!Orders) return 0;
+
+  const now = new Date();
+
+  return Orders.reduce((total: number, order: OrdersProps) => {
+    const orderDate = new Date(order.createdAt);
+
+    const isSameMonth =
+      orderDate.getFullYear() === now.getFullYear() &&
+      orderDate.getMonth() === now.getMonth();
+
+    if (isSameMonth) {
+      return total + Number(order.total_price || 0);
+    }
+
+    return total;
+  }, 0);
+}, [Orders]);
+
   return (
     <section className="space-y-4 mt-8">
         <div className="rounded-xl bg-white p-6 shadow-md">
                 <h3 className="text-lg font-bold mb-4">خلاصه هزنیه ها براساس ماه</h3>
                 {/* Not orders */}
-                {/* <p className="text-muted-foreground text-center py-8">هنوز سفارش تکمیل شده ای وجود نداره</p>             */}
+                {Orders?.length === 0 && (
+             <p className="text-muted-foreground text-center py-8">هنوز سفارش تکمیل شده ای وجود نداره</p>      
+                )}
 
                 <div className="space-y-3">
                     <div className="flex items-center justify-between bg-muted/50 rounded-lg p-4">
                         <div>
-                            <span className="font-medium">مهر</span>
+                            <span className="font-medium">
+                                {new Date().toLocaleDateString('fa-IR',{month:'long'})}
+                            </span>
                             <p className="text-sm text-muted-foreground">
-                                {(12).toLocaleString('fa-IR')}
+                                {new Date().toLocaleDateString('fa-IR',{day:'numeric'})}
                             </p>
                         </div>
                         <span className="font-bold text-copper">
-                            {(789000).toLocaleString('fa-IR')} تومان
+                            {(monthlyTotal).toLocaleString('fa-IR')} تومان
                         </span>
                     </div>
                 </div>
@@ -26,22 +54,26 @@ const CustomerCostDetails:React.FC = ()=>{
         <div className="rounded-xl bg-white p-6 shadow-md">
             <h3 className="text-lg font-bold mb-4">آخرین خرید ها</h3>
             {/* not buy */}
-            {/* <p className="text-muted-foreground text-center py-8">
+            {Orders?.length === 0 && (
+            <p className="text-muted-foreground text-center py-8">
                 هنوز خریدی انجام نشده
-            </p> */}
+            </p> 
+            )}
 
             <div className="space-y-4">
-                <div className="flex items-center justify-between bg-muted/50 rounded-lg p-4">
+                {Orders?.slice(0 , 3).map((order:OrdersProps)=>(
+                <div className="flex items-center justify-between bg-muted/50 rounded-lg p-4" key={order.id}>
                     <div>
-                    <span className="font-medium">سفارش : ORD-1766228790198</span>
+                    <span className="font-medium">سفارش : ORD-{order.id}</span>
                     <p className="text-sm text-muted-foreground">
-                        شعبه ونک - {new Date().toLocaleString('fa-IR')}
+                        شعبه {order.branch.name} - {new Date(order.delivery_date).toLocaleDateString('fa-IR')}
                     </p>
                     </div>
                     <span className="font-bold text-copper">
-                        {(789000).toLocaleString('fa-IR')} تومان
+                        {Number(order.total_price).toLocaleString('fa-IR')} تومان
                     </span>
                 </div>
+                ))}
             </div>
         </div>
     </section>
